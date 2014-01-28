7c6d262f04093e4ae47f9de5fa56eb5a1ca086f9
Hamilton Chan <hamiltonchanjr@gmail.com>
2014-01-28 15:52:59 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Beersontap::Application.config.secret_key_base = 'bec1a62000e89a047f945a96240db5a460a4917d15acbd0a3ed4dcd5bf08bbcfd2f97a2537cb8986c8437fe49b6560268c9ca5422d514466108d1cccee012a23'
